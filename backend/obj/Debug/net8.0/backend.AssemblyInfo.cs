fb5d0e1dcc39aac4a610155331f2b9c2489ea69f
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2024-04-28 11:31:06 -0300

    add vercel.json file

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f19188694314ff16f126a47957181377666f20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab5ba11637255e0295b85569ba50e7a7345988d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
